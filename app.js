9d5a09831967eeb2a0771db08c89a5bf5f49cdf2
Asadov <nikosdov555@gmail.com>
2023-09-16 14:17:03 +0400

    asfaf

--- app.js
+++ app.js
@@ -1,61 +1,52 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express=require("express");
+const mysql=require("mysql2");
+var cors = require('cors')
 
-app.get("/", (req, res) => res.type('html').send(html));
+const app=express();
+app.use(cors())
+let connection = mysql.createConnection({
+    host: "bpgumhqgvazuqzn3tmmg-mysql.services.clever-cloud.com",
+    user: "uzkehuixmxeaspfg",
+    password: "7dI3hNnHUe05ycEgb91d",
+    database: "bpgumhqgvazuqzn3tmmg",
+  });
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+  
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  app.get ("/Persons", (req,res)=>{
+
+    connection.query("select * from Persons", function (err, result, fields) {
+      console.log(err);
+      console.log(result);
+      console.log(fields);
+        res.send(result);
+    }
+
+   
+  );
+  
+});
+
+//  app.listen(3000);
+
+ app.post("/Persons/", (req, res) => {
+  let obj = req.body;
+  connection.query(
+    `INSERT INTO Persons (ID, LastName, FirstName )
+    VALUES ("${obj.ID}", "${obj.LastName}", "${obj.FirstName}")`,
+    function (err, result, fields) {
+      //   console.log(result);
+      //   app.get("/student", function (req, res) {
+      //     res.send(result);
+      //   });
+    }
+  );
+  connection.query("select * from Persons", function (err, result, fields) {
+    //   console.log(err);
+    console.log(result);
+    res.send(result);
+  });
+});
+
+app.listen(process.env.PORT || 3000);
